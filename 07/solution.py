dc7e10539adf1cf6af33da5ae88c0395f3cd1af8
Joel Clarke <joel.clarke273@gmail.com>
2022-12-16 00:59:33 +0000

    Complete day 7

--- 07/solution.py
+++ 07/solution.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from treenode import Node, Directory, Leaf
 
 FILENAME = "07/input.txt"
@@ -9,7 +9,7 @@ def node_gen(commands: List[str]):
         yield commands.pop(0)
 
 
-def cd_mode(path: str, current_node: Node):
+def cd(path: str, current_node: Node) -> Node:
     if path == "/":
         while current_node.name != "/":
             current_node = current_node.parent
@@ -23,63 +23,88 @@ def cd_mode(path: str, current_node: Node):
     return current_node
 
 
-def ls_mode(commands: List[str], current_node: Directory):
+def ls(commands: List[str], current_node: Directory) -> Node:
     children = [child.split() for child in node_gen(commands)]
-    children = {
+
+    return {
         name:
         Directory(name, current_node) if prefix == "dir"
         else Leaf(name, current_node, int(prefix))
         for prefix, name in children
     }
+
+
+def construct_tree(command_list: List[str]) -> Node:
+    current_node = Directory("/", None)
     
-    current_node.children = children
+    while command_list:
+        command = command_list.pop(0).split()
+        
+        if command[1] == "cd":
+            current_node = cd(command[2], current_node)
+            
+        elif command[1] == "ls":
+            current_node.children = ls(command_list, current_node)
+      
+    return cd("/", current_node)
 
 
-def small_sum(root: Directory) -> int:
+def small_sum(root: Directory, limit = 1e5) -> int:
     total = 0
-    limit = 1e5
     nodes = [root]
     
+    # bfs
+    # Probably not as time efficient as possible since sizes are worked out multiple times
+    # (once for each node between root and current inclusive)
     while nodes:
         current = nodes.pop(0)
         size = current.size()
         total += size if size <= limit else 0
         
-        if isinstance(current, Directory):
-            nodes.extend([
-                child for child in current.children.values()
-                if isinstance(child, Directory)
-            ])
+        nodes.extend([
+            child for child in current.children.values()
+            if isinstance(child, Directory)
+        ])
     
     return total
 
 
-def part_1(contents: List[str]):
-    current_node = Directory("/", None)
+def big_collection(root: Directory, start: int, minimum=0):
+    nodes = [root]
+    smallest = start
     
-    # Initialise tree
-    while contents:
-        command = contents.pop(0).split()
+    while nodes:
+        current = nodes.pop(0)
+        size = current.size()
+        smallest = size if size >= minimum and size < smallest else smallest
         
-        if command[1] == "cd":
-            current_node = cd_mode(command[2], current_node)
-            
-        elif command[1] == "ls":
-            ls_mode(contents, current_node)
+        nodes.extend([
+            child for child in current.children.values()
+            if isinstance(child, Directory)
+        ])
     
-    current_node = cd_mode("/", current_node)
-    
-    return small_sum(current_node)
+    return smallest
 
 
-def part_2(contents):
-    return ""
+def part_1(contents: List[str]) -> Tuple[Node, int]:    
+    return (
+        current_node := construct_tree(contents),
+        small_sum(current_node)
+    )
+
+
+def part_2(tree: Node) -> int:
+    tree_size = tree.size()
+    minimum = int(tree_size - 4e7)
+    
+    return big_collection(tree, tree_size, minimum)
 
 
 if __name__ == "__main__":
     with open(FILENAME) as file:
         contents = file.read().splitlines()
     
-    print(part_1(contents))
-    print(part_2(contents))
-
+    tree, size =part_1(contents)
+    print(size)
+    
+    print(part_2(tree))
