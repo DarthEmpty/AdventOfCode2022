c23fa9cd2466ed8126113060ceaec3b1e886c54c
Joel Clarke <joel.clarke273@gmail.com>
2022-12-17 00:55:03 +0000

    Refactor part 1 (and start part 2)

--- 08/solution.py
+++ 08/solution.py
@@ -4,28 +4,51 @@ from typing import List
 FILENAME = "08/input.txt"
 
 
-def is_visible(row: int, column: int, grid: np.ndarray):
+def setup_grid(contents: List[str]) -> np.ndarray:
+    return np.array([list(row) for row in contents]).astype(int)
+
+
+def cardinals(row: int, column: int, grid: np.ndarray):
+    # North and West are sliced in reverse so that all values
+    # are ordered by distance from grid[row, column]
+    
+    return [
+        grid[row - 1::-1, column],   # North
+        grid[row + 1:, column],      # South
+        grid[row, column - 1::-1],   # West
+        grid[row, column + 1:]       # East
+    ]
+
+
+# def visible_trees(current_tree, other_trees):
+    
+
+
+def is_visible(row: int, column: int, grid: np.ndarray) -> bool:
     # If either row or column is 0, then the tree is visible
     if not (row and column):
         return True
     
-    tree = grid[row, column]
-    cardinal_dirs = [
-        grid[:row, column],      # North
-        grid[row + 1:, column],  # South
-        grid[row, :column],      # West
-        grid[row, column + 1:]   # East
-    ]
-    
     # The tree is visible if it's bigger than ALL trees in ANY direction
     return any(
-        all(tree > other for other in direction)
-        for direction in cardinal_dirs
+        all(grid[row, column] > direction)
+        for direction in cardinals(row, column, grid)
     )
-    
 
-def part_1(contents: List[str]):
-    grid = np.array([list(row) for row in contents]).astype(int)      
+
+def scenic_score(row: int, column: int, grid: np.ndarray) -> int:
+    # Trees on the edge have a score of 0
+    if not (row and column):
+        return 0
+    
+    # for direction in cardinals(row, column, grid)
+    
+    
+    # return np.prod(cardinals_to_boundaries)
+
+
+def part_1(contents: List[str]) -> int:
+    grid = setup_grid(contents)      
     
     return len([
         True for row, column in np.ndindex(grid.shape)
@@ -33,8 +56,13 @@ def part_1(contents: List[str]):
     ])
 
 
-def part_2(contents):
-    return ""
+def part_2(contents: List[str]) -> int:
+    grid = setup_grid(contents)
+    
+    return max(
+        scenic_score(row, column, grid)
+        for row, column in np.ndindex(grid.shape)
+    )
 
 
 if __name__ == "__main__":
@@ -42,5 +70,5 @@ if __name__ == "__main__":
         contents = file.read().splitlines()
     
     print(part_1(contents))
-    print(part_2(contents))
+    # print(part_2(contents))
 
