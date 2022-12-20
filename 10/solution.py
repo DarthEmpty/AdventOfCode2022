507e042a5d184b6963d72dc134ef0d5fae7ba559
Joel Clarke <joel.clarke273@gmail.com>
2022-12-20 16:20:57 +0000

    Complete day 10

--- 10/solution.py
+++ 10/solution.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple, Union, Generator
+import numpy as np
+from typing import List, Tuple, Generator
 
 FILENAME = "10/input.txt"
 
@@ -11,29 +12,48 @@ def interpreted(commands: List[str]) -> Generator[Tuple[int, int], None, None]:
             yield 2, int(current.split()[1])
         
         else:  # `current` is "noop"
-            yield 1, 0
+            yield 1, 0   
 
 
-def part_1(contents: List[str]) -> int:
+def execute(commands: List[str]) -> List[int]:
     register = 1
-    history = [0]  # Makes list 1-indexed to match question
+    history = []
     
-    for duration, value in interpreted(contents):
+    for duration, value in interpreted(commands):
         history.extend(register for _ in range(duration))
         register += value
     
-    history.append(register)
+    return history
+
+
+def margin(centre: int, error: int) -> range:
+    return range(centre - error, centre + error + 1)
+
+
+def part_1(contents: List[str]) -> int:
+    history = execute(contents)    
     
-    return sum(cycle * history[cycle] for cycle in range(20, 221, 40))
+    return sum(
+        (cycle + 1) * history[cycle]
+        for cycle in range(19, 220, 40)
+    )
 
 
-def part_2(contents: List[str]) -> int:
-    return 0
+def part_2(contents: List[str]) -> List[List[str]]:
+    history = execute(contents)
+    crt = [
+        "#" if cycle % 40 in margin(history[cycle], 1)
+        else "." for cycle in range(len(history))
+    ]
+    
+    return np.reshape(crt, (6, 40))
 
 
 if __name__ == "__main__":
     with open(FILENAME) as file:
         contents = file.read().splitlines()
+    
+    np.set_printoptions(linewidth=163)
     
     print(part_1(contents.copy()))
     print(part_2(contents))
