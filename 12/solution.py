3348fc4da6d03c07928853c5632a2a78f80a16de
Joel Clarke <joel.clarke273@gmail.com>
2022-12-23 15:23:07 +0000

    Refactor into a bfs method

--- 12/solution.py
+++ 12/solution.py
@@ -1,62 +1,64 @@
-from typing import List
+from typing import List, Callable
 from grid_map import GridMap, Node, contains_coords
 
 FILENAME = "12/input.txt"
 
 
-def part_1(contents: List[str]) -> int:
-    letter_map = GridMap(contents)
-    letter_map.visit(letter_map.start)
-    nodes = [Node(letter_map.start)]
+def bfs(grid_map: GridMap, start_node: Node, end_condition: Callable) -> int:
+    grid_map.visit(start_node.coords)
+    nodes = [start_node]
     
     while nodes:
         # Consider the neighbours of one of the nodes that we're at
         current_node = nodes.pop(0)
-        neighbours = letter_map.reachable_neighbours(current_node.coords)
-        
-        # Return the number of steps if we see the end!
-        if contains_coords(letter_map.end, neighbours):
-            current_node.add_children(letter_map.end)
-            return current_node.children[0].count_steps()
-        
-        # Otherwise, visit all the neighbours by adding them to the queue
-        letter_map.visit(neighbours)
-        current_node.add_children(neighbours)
-        nodes.extend(current_node.children)
-    
-    # Something went wrong!
-    print("We ran out of nodes to explore... here's what we did find:")
-    print(letter_map)    
-    return -1
-
-
-# TODO: Similar to Part 1 but in reverse! (From End to an "a")
-def part_2(contents: List[str]) -> int:
-    letter_map = GridMap(contents, backwards=True)
-    letter_map.visit(letter_map.end)
-    nodes = [Node(letter_map.end)]
-    
-    while nodes:
-        # Consider the one of the nodes that we're at
-        current_node = nodes.pop(0)
         
         # Return the number of steps if we're at the end!
-        if letter_map.height_of(current_node.coords) == "a":
+        if end_condition(current_node):
             return current_node.count_steps()
         
         # Otherwise, visit all the neighbours by adding them to the queue
-        neighbours = letter_map.reachable_neighbours(current_node.coords)
-        letter_map.visit(neighbours)
+        neighbours = grid_map.reachable_neighbours(current_node.coords)
+        grid_map.visit(neighbours)
         current_node.add_children(neighbours)
         nodes.extend(current_node.children)
     
-    return letter_map
+    # Something went wrong!  
+    raise Exception(
+        "Ran out of nodes to explore, but here's what we did find:",
+        str(grid_map)
+    )
+    
+
+def part_1(contents: List[str]) -> int:
+    letter_map = GridMap(contents)
+    
+    return bfs(
+        letter_map,
+        Node(letter_map.start),
+        lambda current: contains_coords(current.coords, letter_map.end)
+    )
+
+
+# Similar to Part 1 but in reverse! (From End to an "a")
+def part_2(contents: List[str]) -> int:
+    letter_map = GridMap(contents, backwards=True)
+    
+    return bfs(
+        letter_map,
+        Node(letter_map.end),
+        lambda current: letter_map.height_of(current.coords) == "a"
+    )
 
 
 if __name__ == "__main__":
     with open(FILENAME) as file:
         contents = file.read().splitlines()
+
+    try:
+        print(part_1(contents))
+        print(part_2(contents))
     
-    print(part_1(contents))
-    print(part_2(contents))
+    except Exception as e:
+        print(e.args[0])
+        print(e.args[1])
 
